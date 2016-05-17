ae832ace3f1e76f0ca502df9aec30e27162ba7fd
Lucien Du <david.xelnaga@gmail.com>
2016-05-16 19:34:56 -0700

    Code base cleanedup

--- src/memDB/Record.h
+++ src/memDB/Record.h
@@ -5,16 +5,19 @@
 
 namespace memDB
 {
-	struct Record
+	namespace detail
 	{
-		using TimePoint = std::chrono::high_resolution_clock::time_point;
-		int posX;
-		int posY;
-		TimePoint time;
-		Record(int x, int y, TimePoint t)
-			:posX(x), posY(y), time(t)
+		struct Record
 		{
+			using TimePoint = std::chrono::high_resolution_clock::time_point;
+			int posX;
+			int posY;
+			TimePoint time;
+			Record(int x, int y, TimePoint t)
+				:posX(x), posY(y), time(t)
+			{
 
-		}
-	};
+			}
+		};
+	}
 }
